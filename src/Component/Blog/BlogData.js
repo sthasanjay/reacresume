aea658e13adfd79df32633f1c8b70e233ab7a3ad
sanjay <sanjaybijay105@gmail.com>
2021-07-12 12:46:29 +0545

    Blog page design is done

---
+++ src/Component/Blog/BlogData.js
@@ -0,0 +1,56 @@
+import blog1 from '../../img/blogs/blog1.svg';
+import blog2 from '../../img/blogs/blog2.svg';
+import blog3 from '../../img/blogs/blog3.svg';
+import blog4 from '../../img/blogs/blog4.svg';
+import blog5 from '../../img/blogs/blog5.svg';
+
+
+
+
+const BlogData = [
+    {
+        id:1,
+        img:blog1,
+        title:'UI/Ux Design Starter with Figma',
+        link:'https://www.facebook.com/',
+        data:'21',
+        month:'Mar',
+        year:'2020'
+
+    },
+    {
+        id:2,
+        img:blog2,
+        title:'UI/Ux Design Starter with Figma',
+        link:'https://www.facebook.com/',
+        data:'25',
+        month:'Mar',
+        year:'2020'
+
+    },
+   
+    {
+        id:3,
+        img:blog3,
+        title:'UI/Ux Design Starter with Figma',
+        link:'https://www.facebook.com/',
+        data:'21',
+        month:'Mar',
+        year:'2020'
+
+    },
+    {
+        id:4,
+        img:blog4,
+        title:'UI/Ux Design Starter with Figma',
+        link:'https://www.facebook.com/',
+        data:'21',
+        month:'Mar',
+        year:'2020'
+
+    },
+  
+]
+
+
+export default BlogData;
--- src/Pages/Blog.jsx
+++ src/Pages/Blog.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
-
+import React from 'react';
+import BlogData from '../Component/Blog/BlogData';
+import Title from '../Component/About/Title';
 const Blog = () => {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (<>
+  <div className = "b-title">
+    <Title title ={'Blog'} span = {'Blog'}/>
+    </div>
+    <div className = "blogPage">
+   
+      {
+        BlogData.map((value)=>{
+          return <div className = "blog" key = "value.id">
+            <div className = "blog-content">
+              <img src = {value.img} alt = ""/>
+              <a href = {value.link} className = "blog-link">{value.title}</a>
+              <div className = "date">
+                <p></p>
+              </div>
+            </div>
+          </div>
+        })
+      }
+    </div>
+    </>
+  )
+  ;
 }
 
-export default Blog
+export default Blog;
